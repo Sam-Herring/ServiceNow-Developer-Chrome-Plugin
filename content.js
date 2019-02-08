6c3d15f853d82d65737a475e8cf59e3bc3fa011c
sherring@usc.edu.au <sherring@usc.edu.au>
2019-02-08 15:32:39 +1000

    Made the generation of the DOM Element more modular instead of injecting raw HTML.

--- content.js
+++ content.js
@@ -1,6 +1,32 @@
+function createUpdateSetListIcon(){
+    //Get the appropriate location (NavPage Header Content)
+    var element = document.getElementsByClassName('navpage-pickers navpage-header-content');
+    var innerElement = element[0];
+
+    //Generate the DOM Element to be injected
+
+    //Div Generation
+    var customDiv = document.createElement('div');
+    customDiv.className = 'form-group picker ng-isolate-scope';
+
+    //Icon Generation
+    var icon = document.createElement('a');
+    icon.className = 'icon-all-apps btn btn-icon ng-scope';
+    icon.href = '/sys_update_set_list.do';
+    icon.target = 'gsft_main';
+
+    //Hover Text Generation
+    var span = document.createElement('span')
+    span.className = 'sr-only';
+    span.innerHTML = 'View All Update Sets';
+
+    //Element Combination
+    icon.appendChild(span);
+    customDiv.appendChild(icon);
+
+    //Insertion of custom CSS
+    innerElement.insertAdjacentElement('afterbegin', customDiv);
+}
+
 console.log("--- RUNNING SHERRING CUSTOM SN DEV TOOLS ---");
-
-var element = document.getElementsByClassName('navpage-pickers navpage-header-content');
-var innerElement = element[0];
-
-innerElement.insertAdjacentHTML('afterbegin', '<div class="form-group picker ng-isolate-scope"><a ng-if="::inHeader" href="/sys_update_set_list.do" target="gsft_main" class="icon-all-apps btn btn-icon ng-scope" data-toggle="tooltip" data-original-title="" title=""><span class="sr-only">View All Update Sets</span></a></div>');
+createUpdateSetListIcon();
