467d66cd03d6c5c005099024da8bbe5c17f77a9b
sherring@usc.edu.au <sherring@usc.edu.au>
2019-02-12 14:26:41 +1000

    Implemented proper omnibox API interactions to drive per-environment search ability

--- background.js
+++ background.js
@@ -1,8 +1,36 @@
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
-    if(text == 'prod' || text == 'PROD' || text == "Prod"){
-      searchTerm = text.
-      "https://usc.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + text
+    if(text.startsWith("prod ")){
+      //Sperate the potential search terms from the query
+      var textArray = text.split(' ');
+      textArray.shift();
+      var searchTerm = textArray.join(' ');
+
+      //Create the new Search URL in a new Tab
+      var newURL = "https://usc.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(text);
+      chrome.tabs.update({ url: newURL });
+    } else if(text.startsWith("test ")){
+      //Sperate the potential search terms from the query
+      var textArray = text.split(' ');
+      textArray.shift();
+      var searchTerm = textArray.join(' ');
+
+      //Create the new Search URL in a new Tab
+      var newURL = "https://usctest.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(text);
+      chrome.tabs.update({ url: newURL });
+    } else if(text.startsWith("dev ")){
+      //Sperate the potential search terms from the query
+      var textArray = text.split(' ');
+      textArray.shift();
+      var searchTerm = textArray.join(' ');
+
+      //Create the new Search URL in a new Tab
+      var newURL = "https://uscdev.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(text);
+      chrome.tabs.update({ url: newURL });
+    } else {
+      //Just search the result against Production
+    var newURL = "https://usc.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(text);
+      chrome.tabs.update({ url: newURL });
     }
   });
