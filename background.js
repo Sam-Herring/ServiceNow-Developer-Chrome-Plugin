fa5679ed28db89e82d97ffb78597e92b4334dd03
sherring@usc.edu.au <samherring111@gmail.com>
2019-04-03 14:29:51 +1000

    Environment Searching
    Fixed the Environment Searching Flags so they actually work now.

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
+    text = text.toLowerCase();
     if(text.startsWith("prod ")){
       //Sperate the potential search terms from the query
       var textArray = text.split(' ');
@@ -8,7 +9,7 @@ chrome.omnibox.onInputEntered.addListener(
       var searchTerm = textArray.join(' ');
 
       //Create the new Search URL in a new Tab
-      var newURL = "https://usc.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(text);
+      var newURL = "https://usc.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(searchTerm);
       chrome.tabs.update({ url: newURL });
     } else if(text.startsWith("test ")){
       //Sperate the potential search terms from the query
@@ -17,7 +18,7 @@ chrome.omnibox.onInputEntered.addListener(
       var searchTerm = textArray.join(' ');
 
       //Create the new Search URL in a new Tab
-      var newURL = "https://usctest.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(text);
+      var newURL = "https://usctest.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(searchTerm);
       chrome.tabs.update({ url: newURL });
     } else if(text.startsWith("dev ")){
       //Sperate the potential search terms from the query
@@ -26,7 +27,7 @@ chrome.omnibox.onInputEntered.addListener(
       var searchTerm = textArray.join(' ');
 
       //Create the new Search URL in a new Tab
-      var newURL = "https://uscdev.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(text);
+      var newURL = "https://uscdev.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(searchTerm);
       chrome.tabs.update({ url: newURL });
     } else {
       //Just search the result against Production
--- options.js
+++ options.js
@@ -1,23 +1,20 @@
 //Save Environment Prefix to Chrome Storage
 function saveChanges(){
+  console.log("Saving Changes...");
   var environment_prefix = document.getElementById('environment_input');
-  console.log(environment_input.value);
-  if(!environment_prefix.value){
+  if(!environment_prefix){
     message("Error: No Environment Prefix Specified");
     return;
   }
 
-  chrome.storage.local.set({'environment_input': environment_prefix.value, function() {
-    message('Settings Saved');
-  }});
+  console.log("Setting Environment Prefix to " + environment_input.value);
+  chrome.storage.local.set({'environment_input': environment_prefix.value});
 }
 
 //Restores the Populated Prefix using the saved preference
 function restore_options(){
-  chrome.storage.local.get({
-    environment_prefix: ''
-  }, function(items) {
-    document.getElementById('environment_input').value = items.environment_prefix;
+  chrome.storage.local.get(['environment_input'], function(result) {
+    document.getElementById('environment_input').value = result.environment_input;
   });
 }
 
