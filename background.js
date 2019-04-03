a60a8b25b7b369b51ce812dcc715d4260cbd91db
sherring@usc.edu.au <samherring111@gmail.com>
2019-04-03 15:47:06 +1000

    Added the ability to DIRECTLY GO TO A RECORD WHOOP WHOOP

--- background.js
+++ background.js
@@ -1,48 +1,42 @@
+//Sperate the potential search terms from the query
+function textSplitter(text){
+  var textArray = text.split(' ');
+  textArray.shift();
+  var searchTerm = textArray.join(' ');
+  return searchTerm;
+}
+
+function newSearch(environment, searchTerm){
+  var newURL = "https://" + environment + ".service-now.com/text_search_exact_match.do?sysparm_search=" + encodeURIComponent(searchTerm);
+  chrome.tabs.update({ url: newURL });
+}
+
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
     text = text.toLowerCase();
     if(text.startsWith("prod ")){
-      //Sperate the potential search terms from the query
-      var textArray = text.split(' ');
-      textArray.shift();
-      var searchTerm = textArray.join(' ');
+      var searchTerm = textSplitter(text);
+      newSearch("usc", searchTerm);
 
-      //Create the new Search URL in a new Tab
-      var newURL = "https://usc.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(searchTerm);
-      chrome.tabs.update({ url: newURL });
     } else if(text.startsWith("test ")){
-      //Sperate the potential search terms from the query
-      var textArray = text.split(' ');
-      textArray.shift();
-      var searchTerm = textArray.join(' ');
+      var searchTerm = textSplitter(text);
+      newSearch("usctest", searchTerm);
 
-      //Create the new Search URL in a new Tab
-      var newURL = "https://usctest.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(searchTerm);
-      chrome.tabs.update({ url: newURL });
     } else if(text.startsWith("dev ")){
-      //Sperate the potential search terms from the query
-      var textArray = text.split(' ');
-      textArray.shift();
-      var searchTerm = textArray.join(' ');
+      var searchTerm = textSplitter(text);
+      newSearch("uscdev", searchTerm);
 
-      //Create the new Search URL in a new Tab
-      var newURL = "https://uscdev.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(searchTerm);
-      chrome.tabs.update({ url: newURL });
     } else if (text.startsWith("docs ")){
-      //Sperate the potential search terms from the query
-      var textArray = text.split(' ');
-      textArray.shift();
-      var searchTerm = textArray.join(' ');
-
+      var searchTerm = textSplitter(text);
       var newURL = "https://docs.servicenow.com/search?q=" + encodeURIComponent(searchTerm) + "&labels=3";
-        chrome.tabs.update({ url: newURL });
+      chrome.tabs.update({ url: newURL });
+
     } else {
       //Just search the result against Production
-    var newURL = "https://usc.service-now.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D" + encodeURIComponent(text);
-      chrome.tabs.update({ url: newURL });
-    }
-  });
+      var searchTerm = text;
+      newSearch("usc", searchTerm);
+  }});
 
   chrome.commands.onCommand.addListener( function(command) {
       if(command === "save-record"){
