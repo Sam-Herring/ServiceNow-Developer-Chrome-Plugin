9cc5244fe855b8261922e2c06d3752a5ae45e2ab
sherring@usc.edu.au <samherring111@gmail.com>
2019-04-04 13:42:16 +1000

    Added all the necessary components to eventually do external email links quicker, but hit some roadblocks with populating the data automatically; needs some love.

--- background.js
+++ background.js
@@ -6,6 +6,19 @@ function textSplitter(text){
   return searchTerm;
 }
 
+chrome.tabs.onUpdated.addListener(
+  function(tabId, changeInfo, tab) {
+    // read changeInfo data and do something with it
+    // like send the new url to contentscripts.js
+    if (changeInfo.url) {
+      chrome.tabs.sendMessage( tabId, {
+        message: 'urlChanged',
+        url: changeInfo.url
+      })
+    }
+  }
+);
+
 function newSearch(environment, searchTerm){
   var newURL = "https://" + environment + ".service-now.com/nav_to.do?uri=%2Ftext_search_exact_match.do?sysparm_search=" + encodeURIComponent(searchTerm);
   chrome.tabs.update({ url: newURL });
@@ -38,17 +51,17 @@ chrome.omnibox.onInputEntered.addListener(
       newSearch("usc", searchTerm);
   }});
 
-  chrome.commands.onCommand.addListener( function(command) {
-      if(command === "save-record"){
-        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-            url = tabs[0].url;
-            if(url.includes("service-now")){
-              //chrome.tabs.update({ url: url + "return gsftSubmit(this);"});
-              chrome.tabs.executeScript(null, {code:"gsftSubmit(gel('sysverb_update_and_stay'));"},
-                function(results){
-                  console.log(results);
-                });
-            }
-        });
-      }
-  });
+chrome.commands.onCommand.addListener( function(command) {
+    if(command === "save-record"){
+      chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+          url = tabs[0].url;
+          if(url.includes("service-now")){
+            //chrome.tabs.update({ url: url + "return gsftSubmit(this);"});
+            chrome.tabs.executeScript(null, {code:"gsftSubmit(gel('sysverb_update_and_stay'));"},
+              function(results){
+                console.log(results);
+              });
+          }
+      });
+    }
+});
